7f41c09ddbf80b61aa7fb71cd00711a8f9de3830
Jeff Hammel <jhammel@mozilla.com>
2007-03-12 21:11:52 +0000

    fixing minor bugs
    git-svn-id: https://svn.socialplanning.org/svn/topp.utils/trunk@3562 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- topp/utils/uri.py
+++ topp/utils/uri.py
@@ -13,24 +13,29 @@ def uri_re_string(scheme=('http','https')):
     """ returns a regular expression pattern for matching uris """
 
     if scheme:
-        scheme = r'(' + r'|'.join(scheme) + r')' + r'://'
+        if not isinstance(scheme, basestring):
+            scheme = r'(' + r'|'.join(scheme) + r')'
+        scheme += r'://'
     else:
         scheme = r''        
     return ( r'^' + scheme +  relative_uri_re )
 
 url_re = re.compile(uri_re_string(), re.I)
 
-+# indicates if the given string is legitimate url syntax
-+is_url = url_re.match
+def is_url(string):
+    """ indicates if the given string is legitimate url syntax """
+    if re.match(url_re, string):
+        return True
+    return False
 
 def uri_type(string):
     
     uri_types = {'http': uri_re_string('http'),
                  'https': uri_re_string('https'),
                  'ftp': uri_re_string('ftp'),
-                 'file': '^file:///' + relative_uri_re,
+                 'file': r'^file:///' + relative_uri_re,
                  }
 
     for i in uri_types:
-        if re.match(string, uri_types[i]):
+        if re.match(uri_types[i], string):
             return i
