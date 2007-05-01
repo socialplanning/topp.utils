4e180d086ba70518a5a4868961a8088b7e46c27e
Jeff Hammel <jhammel@mozilla.com>
2007-05-01 17:19:41 +0000

    add stdout-writin error message function
    git-svn-id: https://svn.socialplanning.org/svn/topp.utils/trunk@4849 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ topp/utils/error.py
@@ -0,0 +1,15 @@
+def error(msg, parser=None, warning=False):
+    """
+    print an error message
+    in the future, this should log
+    """
+
+    if warning:
+        err_string = "WARNING"
+    else:
+        err_string = "ERROR"
+    print err_string + ":", msg
+    if parser:
+        parser.print_usage()
+    if not warning:
+        sys.exit(1)
