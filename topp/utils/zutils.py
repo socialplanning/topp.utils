ac17c4e07fbb6939dfdd7936cba0e76472d56d78
Whit Morriss <d.w.morriss@gmail.com>
2007-11-09 16:56:49 +0000

    remove cruft imports
    git-svn-id: https://svn.socialplanning.org/svn/topp.utils/trunk@10781 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- topp/utils/zutils.py
+++ topp/utils/zutils.py
@@ -2,10 +2,7 @@
 zopeish utils
 """
 from Acquisition import aq_inner, aq_parent
-from zope.component import adapter, getUtility
-from zope.interface import implementer
-from topp.featurelets.interfaces import IFeatureletRegistry
-from topp.featurelets.interfaces import IFeatureletSupporter
+
 
 def aq_iface(obj, iface):
     obj = aq_inner(obj)
