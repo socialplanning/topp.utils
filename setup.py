98431f37921399489b55afd9c41b611d8ef6f8b5
Whit Morriss <d.w.morriss@gmail.com>
2007-12-10 19:35:27 +0000

    bump version to 0.3 to reflect new module
    git-svn-id: https://svn.socialplanning.org/svn/topp.utils/trunk@11757 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.11'
+version = '0.3'
+
+
+f = open('README.txt')
+readme = "".join(f.readlines())
+f.close()
 
 setup(name='topp.utils',
       version=version,
       description="utility library used for openplans.org",
-      long_description="""\
-""",
+      long_description=readme,
       classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       keywords='topp openplans',
       author='The Open Planning Project',
